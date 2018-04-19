7f0189e66341f92fcb5bdc7ab934e165dd2deaec
Yang Hongbo <465558+sprhawk@users.noreply.github.com>
2018-04-19 16:29:59 +0800

    Render a triangle

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[macro_use]
 extern crate vulkano;
 
+extern crate image;
 extern crate vulkano_win;
 extern crate winit;
-extern crate image;
+
+#[macro_use]
+extern crate vulkano_shader_derive;
 
 use std::sync::Arc;
 
@@ -20,16 +23,56 @@ use vulkano::format::{ClearValue, Format};
 
 use vulkano::image::{Dimensions, StorageImage};
 
-use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer, DynamicState};
 
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::framebuffer::{Framebuffer, Subpass};
 
 use vulkano::instance::debug::DebugCallback;
 
 use vulkano::sync::GpuFuture;
 
+use vulkano::pipeline::GraphicsPipeline;
+use vulkano::pipeline::viewport::Viewport;
+
 use image::{ImageBuffer, Rgba};
 
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+impl_vertex!(Vertex, position);
+
+#[allow(dead_code)]
+mod default_vertex_shader {
+    #[derive(VulkanoShader)]
+    #[ty = "vertex"]
+    #[src = "
+    #version 450
+    layout(location = 0) in vec2 position;
+
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+    "]
+    #[allow(dead_code)]
+    struct Dummy;
+}
+#[allow(dead_code)]
+mod default_fragment_shader {
+    #[derive(VulkanoShader)]
+    #[ty = "fragment"]
+    #[src = "
+    #version 450
+    layout(location = 0) out vec4 f_color;
+
+    void main() {
+        f_color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    "]
+    #[allow(dead_code)]
+    struct Dummy;
+}
 fn main() {
     println!("Hello, Vulkan!");
 
@@ -76,19 +119,101 @@ fn main() {
         Some(queue_family),
     ).unwrap();
 
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+
+    let vertex_shader = default_vertex_shader::Shader::load(device.clone())
+        .expect("Failed to create vertex shader module");
+    let fragment_shader = default_fragment_shader::Shader::load(device.clone())
+        .expect("Failed to create fragment shader module");
+
+    let render_pass = Arc::new(
+        single_pass_renderpass!(device.clone(),
+    attachments: {
+        color: {
+            load: Clear,
+            store: Store,
+            format: Format::R8G8B8A8Unorm,
+            samples: 1,
+        }
+    },
+    pass: {
+        color: [color],
+        depth_stencil: {}
+    }).unwrap(),
+    );
+
+    let framebuffer = Arc::new(
+        Framebuffer::start(render_pass.clone())
+            .add(image.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    let pipeline = Arc::new(
+        GraphicsPipeline::start()
+            .vertex_input_single_buffer::<Vertex>()
+            .vertex_shader(vertex_shader.main_entry_point(), ())
+            .viewports_dynamic_scissors_irrelevant(1)
+            .fragment_shader(fragment_shader.main_entry_point(), ())
+            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+            .build(device.clone())
+            .unwrap(),
+    );
+
+    let dynamic_state = DynamicState {
+        viewports: Some(vec![
+            Viewport {
+                origin: [0.0, 0.0],
+                dimensions: [1024.0, 1024.0],
+                depth_range: 0.0..1.0,
+            },
+        ]),
+        ..DynamicState::none()
+    };
+
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        vec![vertex1, vertex2, vertex3].into_iter(),
+    ).unwrap();
+
     let buffer = CpuAccessibleBuffer::from_iter(
         device.clone(),
         BufferUsage::all(),
         (0..1024 * 1024 * 4).map(|_| 0u8),
     ).expect("Failed to create buffer");
 
-    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family())
-        .unwrap()
-        .clear_color_image(image.clone(), ClearValue::Float([0.0, 0.0, 1.0, 1.0]))
-        .unwrap()
-        .copy_image_to_buffer(image.clone(), buffer.clone()).unwrap()
-        .build()
-        .unwrap();
+    let command_buffer =
+        AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family())
+            .unwrap()
+            .begin_render_pass(
+                framebuffer.clone(),
+                false,
+                vec![[0.0, 0.0, 1.0, 1.0].into()],
+            )
+            .unwrap()
+            .draw(
+                pipeline.clone(),
+                dynamic_state,
+                vertex_buffer.clone(),
+                (),
+                (),
+            ).unwrap()
+            .end_render_pass()
+            .unwrap()
+            .copy_image_to_buffer(image.clone(), buffer.clone())
+            .unwrap()
+            .build()
+            .unwrap();
 
     let finished = command_buffer.execute(queue.clone()).unwrap();
     finished
