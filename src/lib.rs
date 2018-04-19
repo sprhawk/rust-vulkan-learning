634be451cea671b89752b1c7005518a1ab1c0f2b
Yang Hongbo <465558+sprhawk@users.noreply.github.com>
2018-04-19 23:16:57 +0800

    move main code to module

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate vulkano;
+
+extern crate image;
+extern crate vulkano_win;
+extern crate winit;
+
+#[macro_use]
+extern crate vulkano_shader_derive;
+
+
+pub mod slicer_lib;
