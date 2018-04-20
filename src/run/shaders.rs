5889867745353e2d65caef9e4e24c60ca7722a97
Yang Hongbo <465558+sprhawk@users.noreply.github.com>
2018-04-20 21:23:57 +0800

    move shaders to seprate files

--- src/run/shaders.rs
+++ src/run/shaders.rs
@@ -9,14 +9,7 @@ impl_vertex!(Vertex, position);
 pub mod default_vertex_shader {
     #[derive(VulkanoShader)]
     #[ty = "vertex"]
-    #[src = "
-    #version 450
-    layout(location = 0) in vec2 position;
-
-    void main() {
-        gl_Position = vec4(position, 0.0, 1.0);
-    }
-    "]
+    #[path = "src/run/default_vertex.glsl"]
     #[allow(dead_code)]
     struct Dummy;
 }
@@ -24,14 +17,7 @@ pub mod default_vertex_shader {
 pub mod default_fragment_shader {
     #[derive(VulkanoShader)]
     #[ty = "fragment"]
-    #[src = "
-    #version 450
-    layout(location = 0) out vec4 f_color;
-
-    void main() {
-        f_color = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-    "]
+    #[path = "src/run/default_fragment.glsl"]
     #[allow(dead_code)]
     struct Dummy;
 }
